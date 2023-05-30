ff1e5429bdd855d20550152a8f52c5cd65bce705
Kévin Vds <118352151+SiskaKizuato@users.noreply.github.com>
2023-05-30 15:21:49 +0200

    wesh

--- app/views.py
+++ app/views.py
@@ -13,4 +13,24 @@ def index(request):
     return render(request, 'app/index.html')
 
 def portfolio(request):
-    return render(request, 'app/portfolio.html')
+    posts = PortfolioPost.objects.all()[:15]  # pour recup les 15 premiers articles
+    images = [
+        "app/img/portfolio-1.jpg",
+        "app/img/portfolio-2.jpg",
+        "app/img/portfolio-3.jpg",
+        "app/img/portfolio-4.jpg",
+        "app/img/portfolio-5.jpg",
+        "app/img/portfolio-6.jpg",
+        "app/img/portfolio-7.jpg",
+        "app/img/portfolio-8.jpg",
+        "app/img/portfolio-9.jpg",
+        "app/img/portfolio-10.jpg",
+        "app/img/portfolio-2.jpg",
+        "app/img/portfolio-9.jpg",
+        "app/img/portfolio-5.jpg",
+        "app/img/portfolio-6.jpg",
+        "app/img/portfolio-3.jpg",
+    ]
+    for post, image in zip(posts, images):
+        post.image = image
+    return render(request, "app/portfolio.html", {"posts": posts})
